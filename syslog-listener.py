62e39d02fbb7030b4763bc768179e525e40c583c
dozer <dozernz@users.noreply.github.com>
2022-04-14 08:01:06 +1200

    Create syslog-listener.py

---
+++ syslog-listener.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env -S python3 -u
+# we need -u to unbuffer output
+# original source: https://gist.github.com/marcelom/4218010
+
+## Tiny Syslog Server in Python.
+##
+## This is a tiny syslog server that is able to receive UDP based syslog
+## entries on a specified port and save them to a file.
+## That's it... it does nothing else...
+## There are a few configuration parameters.
+
+
+LOG_FILE = 'youlogfile.log'
+HOST, PORT = "0.0.0.0", 514
+USEFILE=False
+
+#
+# NO USER SERVICEABLE PARTS BELOW HERE...
+#
+
+import logging
+import socketserver
+
+logging.basicConfig(level=logging.INFO, format='%(message)s', datefmt='', filename=LOG_FILE, filemode='a')
+
+class SyslogUDPHandler(socketserver.BaseRequestHandler):
+
+    def handle(self):
+        data = bytes.decode(self.request[0].strip())
+        socket = self.request[1]
+        print( "%s" % str(data))
+        if USEFILE != False:
+            logging.info(str(data))
+
+if __name__ == "__main__":
+    try:
+        server = socketserver.UDPServer((HOST,PORT), SyslogUDPHandler)
+        server.serve_forever(poll_interval=0.5)
+    except (IOError, SystemExit):
+        raise
+    except KeyboardInterrupt:
+        print ("Crtl+C Pressed. Shutting down.")
