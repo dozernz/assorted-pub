4bff2eda5727521c35ecf6d34d8bf5feb931eaee
dozer <dozernz@users.noreply.github.com>
2025-12-02 09:02:38 +1300

    Update tmux-log-capture.sh

--- tmux-log-capture.sh
+++ tmux-log-capture.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #captures all the scrollback from every open tmux session/window/pane to files on disk
+#can capture ANSI color etc by adding the -e flag to tmux capture-pane. Can get raw bytes without line reflow by dropping the -J flag.
 
 dir="$HOME/tmuxlogs"; mkdir -p "$dir"
 tmux list-panes -a -F '#{session_name}:#{window_index}.#{pane_index}' |
